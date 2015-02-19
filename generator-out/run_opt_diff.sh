348247549da13a6285f04c71648715517dc6f3b9
Mohammad Dashti <mdashti@gmail.com>
2015-02-20 00:17:29 +0100

    Added exclusion filter for query+optimization combination to run_opt_diff

--- generator-out/run_opt_diff.sh
+++ generator-out/run_opt_diff.sh
@@ -3,6 +3,17 @@
 # This script is meant to generate code for different optimization combinations
 # for a specific query, and will generate the statistics in CSV format
 
+exclude () {
+  KEY="$1"
+  KEY=${KEY//:/_}
+  eval hash"$KEY"='TRUE'
+}
+
+checkExcluded () {
+  KEY="$1"
+  KEY=${KEY//:/_}
+  eval echo '${hash'"$KEY"'#hash}'
+}
 
 GEN_OUT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
@@ -55,12 +66,12 @@ eval "rm -f $RESULT_CSV"
 eval "echo -n 'Query,OptimizationCombinationAccronym,OptimizationCombinationFullName' >> $RESULT_CSV"
 for (( i = 1; i <= $NUMRUNS; i+=1 ))
 do
-    eval "echo -n ',RUN #$i$LAST_CHAR' >> $RESULT_CSV"
+    eval "echo -n ',RUN #$i (ms)' >> $RESULT_CSV"
 done
 if [ "$CHECK_CORRECTNESS" = "TRUE" ]; then
     for (( i = 1; i <= $NUMRUNS; i+=1 ))
     do
-        eval "echo -n ',RUN #$i$LAST_CHAR CHECK (ms)' >> $RESULT_CSV"
+        eval "echo -n ',RUN #$i CHECK' >> $RESULT_CSV"
     done 
 fi
 eval "echo ',TOTAL EXECUTION (sec)' >> $RESULT_CSV"
@@ -80,77 +91,88 @@ do
         START_TIME=$(date +%s)
 
         currentOptsIdxs=${OPTIMIZATION_COMBINATIONS[$idx]}
-        opts=(${currentOptsIdxs//:/ })
-        optsLen=${#opts[@]}
-        currentOpts=""
-        currentOptsAcronym=""
-        currentOptsFullName=""
-        for (( opt = 0; opt < $optsLen; opt+=1 ))
-        do
-            currentOpts="$currentOpts ${optArr[${opts[opt]}]}"
-            currentOptsAcronym="$currentOptsAcronym-${optArrAcronym[${opts[opt]}]}"
-            currentOptsFullName="$currentOptsFullName ${optArr[${opts[opt]}]}"
-        done
-
-        eval "rm -rf $GEN_OUT_DIR/*.out*"
-        eval "rm -rf $GEN_OUT_DIR/*.c"
-
-        eval "sed -i -e 's/val numRuns: scala.Int = [0-9]*/val numRuns: scala.Int = $NUMRUNS/g' $GEN_OUT_DIR/../lego/src/main/scala/Config.scala"
-        eval "sbt ';project legocompiler ;run $DATA_FOLDER $SF $QUERY$LANG $currentOpts'"
-        eval "sed -i -e 's/val numRuns: scala.Int = [0-9]*/val numRuns: scala.Int = 1/g' $GEN_OUT_DIR/../lego/src/main/scala/Config.scala"
-
-        eval "make -C $GEN_OUT_DIR"
-        eval "rm -rf $GEN_OUT_DIR/output_$QUERY*"
-
-        eval "echo -n '$QUERY,$currentOptsAcronym,$currentOptsFullName,' >> $RESULT_CSV"
-        eval "$EXECUTABLE > $GEN_OUT_DIR/output_$QUERY.txt"
-        if [ "$CHECK_CORRECTNESS" = "TRUE" ]; then
-            eval "cat $GEN_OUT_DIR/output_$QUERY.txt | grep Generated | sed 's/Generated code run in //g' | sed 's/ milliseconds.//g' | tr '\n' ',' | rev | cut -c 2- | rev | tr '\n' ',' >> $RESULT_CSV"
-        else
-            eval "cat $GEN_OUT_DIR/output_$QUERY.txt | grep Generated | sed 's/Generated code run in //g' | sed 's/ milliseconds.//g' | tr '\n' ',' | rev | cut -c 2- | rev >> $RESULT_CSV"
-        fi
-        eval "cat $GEN_OUT_DIR/output_$QUERY.txt | sed -e 's/Gen.*/\:/' | awk '/:/{n++}{if(\$1!=\":\") print > (\"$GEN_OUT_DIR/output_$QUERY$UNDERLINE\" n \".txt\") }' n=1"
-
-        if [ "$CHECK_CORRECTNESS" = "TRUE" ]; then
-            for (( i = 1; i <= $NUMRUNS; i+=1 ))
+        EXCLUDED=`checkExcluded $QUERY$UNDERLINE$currentOptsIdxs`
+        if [ "$EXCLUDED" != "TRUE" ]; then
+            opts=(${currentOptsIdxs//:/ })
+            optsLen=${#opts[@]}
+            currentOpts=""
+            currentOptsAcronym=""
+            currentOptsFullName=""
+            for (( opt = 0; opt < $optsLen; opt+=1 ))
             do
-                REF_RESULT_FILE="$GEN_OUT_DIR/../results/$QUERY.result_sf$SF"
-                if [ "$SF" = "8" ]; then
-                    REF_RESULT_FILE="$GEN_OUT_DIR/../results/sf$SF/$QUERY.result"
-                fi
-                if [ ! -f "$GEN_OUT_DIR/output_$QUERY$UNDERLINE$i.txt" ]; then
-                    if [ "$i" = "1" ]; then
-                        for (( k = 1; k <= $NUMRUNS; k+=1 ))
-                        do
-                            eval "echo -n ',' >> $RESULT_CSV"
-                        done
-                        eval "echo -n 'NOT_FOUND,' >> $RESULT_CSV"
+                currentOpts="$currentOpts ${optArr[${opts[opt]}]}"
+                currentOptsAcronym="$currentOptsAcronym-${optArrAcronym[${opts[opt]}]}"
+                currentOptsFullName="$currentOptsFullName ${optArr[${opts[opt]}]}"
+            done
+
+            eval "rm -rf $GEN_OUT_DIR/*.out*"
+            eval "rm -rf $GEN_OUT_DIR/*.c"
+
+            eval "sed -i -e 's/val numRuns: scala.Int = [0-9]*/val numRuns: scala.Int = $NUMRUNS/g' $GEN_OUT_DIR/../lego/src/main/scala/Config.scala"
+            eval "sbt ';project legocompiler ;run $DATA_FOLDER $SF $QUERY$LANG $currentOpts'"
+            eval "sed -i -e 's/val numRuns: scala.Int = [0-9]*/val numRuns: scala.Int = 1/g' $GEN_OUT_DIR/../lego/src/main/scala/Config.scala"
+
+            eval "make -C $GEN_OUT_DIR"
+            eval "rm -rf $GEN_OUT_DIR/output_$QUERY*"
+
+            eval "echo -n '$QUERY,$currentOptsAcronym,$currentOptsFullName,' >> $RESULT_CSV"
+            eval "$EXECUTABLE > $GEN_OUT_DIR/output_$QUERY.txt"
+            if [ "$CHECK_CORRECTNESS" = "TRUE" ]; then
+                eval "cat $GEN_OUT_DIR/output_$QUERY.txt | grep Generated | sed 's/Generated code run in //g' | sed 's/ milliseconds.//g' | tr '\n' ',' >> $RESULT_CSV"
+            else
+                eval "cat $GEN_OUT_DIR/output_$QUERY.txt | grep Generated | sed 's/Generated code run in //g' | sed 's/ milliseconds.//g' | tr '\n' ',' | rev | cut -c 2- | rev >> $RESULT_CSV"
+            fi
+            eval "cat $GEN_OUT_DIR/output_$QUERY.txt | sed -e 's/Gen.*/\:/' | awk '/:/{n++}{if(\$1!=\":\") print > (\"$GEN_OUT_DIR/output_$QUERY$UNDERLINE\" n \".txt\") }' n=1"
+
+            if [ "$CHECK_CORRECTNESS" = "TRUE" ]; then
+                for (( i = 1; i <= $NUMRUNS; i+=1 ))
+                do
+                    REF_RESULT_FILE="$GEN_OUT_DIR/../results/$QUERY.result_sf$SF"
+                    if [ "$SF" = "8" ]; then
+                        REF_RESULT_FILE="$GEN_OUT_DIR/../results/sf$SF/$QUERY.result"
                     fi
-                    eval "echo -n 'NOT_FOUND,' >> $RESULT_CSV"
-                else
-                    if [ ! -f "$REF_RESULT_FILE" ]; then
-                        eval "echo -n 'NO_REF,' >> $RESULT_CSV"
+                    if [ ! -f "$GEN_OUT_DIR/output_$QUERY$UNDERLINE$i.txt" ]; then
+                        if [ "$i" = "1" ]; then
+                            for (( k = 1; k <= $NUMRUNS; k+=1 ))
+                            do
+                                eval "echo -n ',' >> $RESULT_CSV"
+                            done
+                            eval "echo -n 'NOT_FOUND,' >> $RESULT_CSV"
+                        fi
+                        eval "echo -n 'NOT_FOUND,' >> $RESULT_CSV"
                     else
-                        if `diff $GEN_OUT_DIR/output_$QUERY$UNDERLINE$i.txt $REF_RESULT_FILE >/dev/null` ; then
-                            eval "echo -n 'OK,' >> $RESULT_CSV"
+                        if [ ! -f "$REF_RESULT_FILE" ]; then
+                            eval "echo -n 'NO_REF,' >> $RESULT_CSV"
                         else
-                            eval "echo -n 'WRONG_RESULT,' >> $RESULT_CSV"
+                            if `diff $GEN_OUT_DIR/output_$QUERY$UNDERLINE$i.txt $REF_RESULT_FILE >/dev/null` ; then
+                                eval "echo -n 'OK,' >> $RESULT_CSV"
+                            else
+                                eval "echo -n 'WRONG_RESULT,' >> $RESULT_CSV"
+                            fi
                         fi
                     fi
-                fi
-            done
+                done
+            fi
+
+            END_TIME=$(date +%s)
+            EXEC_TIME=$(( $END_TIME - $START_TIME ))
+            eval "echo '$EXEC_TIME' >> $RESULT_CSV"
+
+            eval "rm -f $GEN_OUT_DIR/result.csv"
+            eval "cp $RESULT_CSV $GEN_OUT_DIR/result.csv"
+
+            eval "rm -rf $GEN_OUT_DIR/output_$QUERY*"
+
+            eval "mv $EXECUTABLE $ARCHIVE_DIR/$QUERY$currentOptsAcronym.out"
+            eval "mv $GEN_OUT_DIR/$QUERY.c $ARCHIVE_DIR/$QUERY$currentOptsAcronym.c"
+
+            # clenaup
+            if [ "`uname`" == "Linux" ]; then
+                sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches' 
+            else
+                #assume darwin
+                sudo purge
+            fi
         fi
-
-        END_TIME=$(date +%s)
-        EXEC_TIME=$(( $END_TIME - $START_TIME ))
-        eval "echo '$EXEC_TIME' >> $RESULT_CSV"
-
-        eval "rm -f $GEN_OUT_DIR/result.csv"
-        eval "cp $RESULT_CSV $GEN_OUT_DIR/result.csv"
-
-        eval "rm -rf $GEN_OUT_DIR/output_$QUERY*"
-
-        eval "mv $EXECUTABLE $ARCHIVE_DIR/$QUERY$currentOptsAcronym.out"
-        eval "mv $GEN_OUT_DIR/$QUERY.c $ARCHIVE_DIR/$QUERY$currentOptsAcronym.c"
     done
 done
