9a25a8b395ecbe8ac366241a6e91b92857196800
Mohammad Dashti <mdashti@gmail.com>
2015-02-24 07:49:43 +0100

    Added memusg.sh for measuring the peak memory usage of a query

---
+++ generator-out/memusg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+peak=0
+sleep 0.1
+while true; do
+  sleep 0.05
+  pid=$(ps aux | grep /data/lab/dashti/.*Q.*.out | awk '{if($11 ~ "^/data") print $2}')
+  if [[ "$pid" =~ ^\ +$ ]]; then
+  	break
+  else
+    sample="$(ps -o rss= $pid 2> /dev/null)" || break
+    re='^[0-9]+$'
+    if [[ "$sample" =~ $re ]]; then
+      if [[ "$sample" -gt "$peak" ]]; then peak="$sample"; fi;
+    fi
+  fi
+done
+echo "Peak Memory: $peak"
