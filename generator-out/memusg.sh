6587ca9647b71bfa8ac5f59403b5ec6cebf35722
Mohammad Dashti <mdashti@gmail.com>
2015-02-25 03:04:57 +0100

    Fixed memusg for the cases that the process already finished its execution

--- generator-out/memusg.sh
+++ generator-out/memusg.sh
@@ -4,7 +4,7 @@ sleep 0.1
 while true; do
   sleep 0.05
   pid=$(ps aux | grep /data/lab/dashti/.*Q.*.out | awk '{if($11 ~ "^/data") print $2}')
-  if [[ "$pid" =~ ^\ +$ ]]; then
+  if [[ "$pid" =~ ^\ +$ || "$pid" =~ ^$ ]]; then
   	break
   else
     sample="$(ps -o rss= $pid 2> /dev/null)" || break
