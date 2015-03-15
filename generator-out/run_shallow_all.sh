16f76ddc017d1d9f6c9e432a6139fedee47f1116
Mohammad Dashti <mdashti@gmail.com>
2015-03-15 13:10:11 +0100

    Added the script for running all shallow queries based on the config file.

---
+++ generator-out/run_shallow_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+GEN_OUT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+eval "cd $GEN_OUT_DIR"
+
+#reading parameters from config file: DATA_FOLDER
+CONFIG_FILE="run_config.cfg"
+if [ ! -f "$GEN_OUT_DIR/$CONFIG_FILE" ]; then
+    echo "Config file does not exist."
+    echo "You can rename run_config.example.cfg to $CONFIG_FILE and change the parameters inside."
+    exit 1
+fi
+source $CONFIG_FILE
+
+for SF in "${SF_ARR[@]}"
+do
+	echo "Started running shallow for SF=$SF"
+	eval "./run_shallow.sh $DATA_FOLDER $SF"
+	echo "Finished running shallow for SF=$SF"
+done
