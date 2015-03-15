61c97e095fa79efe73fc1bd6fbe6dda0b13a1558
Mohammad Dashti <mdashti@gmail.com>
2015-03-15 13:24:35 +0100

    Added missing run_shallow to develop branch.

---
+++ generator-out/run_shallow.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCALA_PATH="/sdata/home/dashti/scala-2.11.5/bin"
+HOME="/data/home/dashti"
+
+GEN_OUT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+eval "cd $GEN_OUT_DIR"
+
+END=22
+START=1
+
+if [ $# -ne 2 ]; then
+    echo "Invalid number of command line arguments."
+    echo "USAGE: ./run_scala.sh <DATA_FOLDER> <SF>"
+    exit
+fi
+
+rm -rf bin
+mkdir bin
+CPATH=$HOME/ivy2/local/lego-core/lego-core_2.11/0.1-SNAPSHOT/jars/lego-core_2.11.jar:$HOME/ivy2/local/ch.epfl.data/pardis-library_2.11/0.1-SNAPSHOT/jars/pardis-library_2.11.jar:$HOME/ivy2/local/ch.epfl.data/pardis-core_2.11/0.1-SNAPSHOT/jars/pardis-core_2.11.jar
+cd ..
+echo "Now running them"
+for (( i = $START; i <= $END; i+=1 )); do
+	
+    $SCALA_PATH/scala -J-Xmx100g -J-Xms100g -classpath $CPATH ch.epfl.data.legobase.MiniDB $1 $2 "Q"$i
+done
+cd - 1> /dev/null 2> /dev/null
+echo "Done!"
