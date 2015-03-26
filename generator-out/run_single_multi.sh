6fb73d3ef79901abc5525a88e25d4472fed54cb0
Florian Chlan <florian@chlan.net>
2015-03-26 13:15:26 +0100

    Edited bash-scripts for easy testing

--- generator-out/run_single_multi.sh
+++ generator-out/run_single_multi.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-END=2
-START=1
-
+QUERY=1
+NUM=5
 if [ $# -ne 2 ]; then
     echo "Invalid number of command line arguments."
     echo "USAGE: ./run_scala.sh <DATA_FOLDER> <SF>"
@@ -11,15 +10,13 @@ fi
 rm -rf bin
 mkdir bin
 CPATH=$HOME/.ivy2/local/lego-core/lego-core_2.11/0.1-SNAPSHOT/jars/lego-core_2.11.jar:$HOME/.ivy2/local/ch.epfl.data/pardis-library_2.11/0.1-SNAPSHOT/jars/pardis-library_2.11.jar:$HOME/.ivy2/local/ch.epfl.data/pardis-core_2.11/0.1-SNAPSHOT/jars/pardis-core_2.11.jar
-for (( i = $START; i <= $END; i+=1 )); do
-	mkdir bin/Q$i
-	echo "Compiling Q"$i
-	$SCALA_PATH/scalac "Q"${i}".scala" -classpath $CPATH -d bin/Q$i
-done
+	mkdir bin/Q$QUERY
+	echo "Compiling Q"$QUERY
+	$SCALA_PATH/scalac "Q"${QUERY}".scala" -classpath $CPATH -d bin/Q$QUERY
 cd ..
 echo "Now running them"
-for (( i = $START; i <= $END; i+=1 )); do
-    $SCALA_PATH/scala -J-Xmx2g -J-Xms2g -classpath generator-out/bin/Q$i:$CPATH ch.epfl.data.legobase.Q$i $1 $2 "Q"$i
+for (( i = 1; i <= $NUM; i+=1 )); do
+    $SCALA_PATH/scala -J-Xmx2g -J-Xms2g -classpath generator-out/bin/Q$QUERY:$CPATH ch.epfl.data.legobase.Q$QUERY $1 $2 "Q"$QUERY
 done
 cd - 1> /dev/null 2> /dev/null
 echo "Done!"
