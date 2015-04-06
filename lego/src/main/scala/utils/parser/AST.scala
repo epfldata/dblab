6411756041134f5b6ec7342bdf9677eda116b2d0
Florian Chlan <florian@chlan.net>
2015-04-06 19:55:24 +0200

    Implementing SQL parser; WIP

--- lego/src/main/scala/utils/parser/AST.scala
+++ lego/src/main/scala/utils/parser/AST.scala
@@ -3,29 +3,125 @@ package legobase
 package utils
 package parser
 
-class SelectStatement(sl: SelectList, te: TableExpression)
+/**
+ * AST for SQL select statement.
+ * Based on: https://github.com/stephentu/scala-sql-parser
+ */
+case class SelectStatement(projections: Projections,
+                          relations: Option[Seq[Relation]],
+                          where: Option[Expression],
+                          groupBy: Option[SqlGroupBy],
+                          orderBy: Option[SqlOrderBy])
 
-class SelectList
-case class AllColumns() extends SelectList
-case class ColumnList(cols: List[ValueExpression]) extends SelectList
+trait Projections extends Node
+case class ExpressionProjections(lst: Seq[(Expression, Option[String])]) extends Projection
+case class AllColumns()
 
-trait ValueExpression
-class BooleanValueExpression() with ValueExpression
-    case class BooleanTerm() extends BooleanValueExpression
-    case class ORExpression(ex: BooleanValueExpression, te: BooleanTerm) extends BooleanValueExpression    
-class RowValueExpression() with ValueExpression
-class CommonValueExpression() with ValueExpression
+trait Expression extends Node {
+  def getType: DataType = UnknownType
+  def isLiteral: Boolean = false
 
-class TableExpression(from: FromClause, where: WhereClause, group: GroupClause, order: OrderClause)
+  // is the r-value of this expression a literal?
+  def isRValueLiteral: Boolean = isLiteral
 
-class FromClause(tables: List[TableReference])
+  // (col, true if aggregate context false otherwise)
+  // only gathers fields within this context (
+  // wont traverse into subselects )
+  def gatherFields: Seq[(FieldIdent, Boolean)]
+}
 
-class TableReference
-case class PrimaryTable(name: String) extends TableReference
+trait BinaryOperator extends Expression {
+  val left: Expression
+  val right: Expression
 
+  override def isLiteral = left.isLiteral && right.isLiteral
+  def gatherFields = left.gatherFields ++ right.gatherFields  
+}
+case class Or(left: Expression, right: Expression) extends BinaryOperator
+case class And(left: Expression, right: Expression) extends BinaryOperator
 
-class OrderClause(orderKeys: List[OrderKey])
+trait EqualityOperator extends BinaryOperator
+case class Equals(left: Expression, right: Expression) extends EqualityOperator
+case class NotEquals(left: Expression, right: Expression)
 
-class OrderKey
-case class AscendingOrderKey(key: ValueExpression) extends OrderKey
-case class DescendingOrderKey(key: ValueExpression) extends OrderKey
+trait InEqualityOperator extends BinaryOperator
+case class LessOrEqual(left: Expression, right: Expression) extends InEqualityOperator
+case class LessThan(left: Expression, right: Expression) extends InEqualityOperator
+case class GreaterOrEqual(left: Expression, right: Expression) extends InEqualityOperator
+case class GreaterThan(left: Expression, right: Expression) extends InEqualityOperator
+case class In(elem: Expression, set: Seq[Expression], negate: Boolean) extends Expression {
+  override def isLiteral =
+    elem.isLiteral && set.filter(e => !e.isLiteral).isEmpty
+  def gatherFields =
+    elem.gatherFields ++ set.flatMap(_.gatherFields)
+}
+case class Like(left: Expression, right: Expression, negate: Boolean) extends BinaryOperator
+case class Add(left: Expression, right: Expression) extends BinaryOperator
+case class Subtract(left: Expression, right: Expression) extends BinaryOperator
+case class Multiply(left: Expression, right: Expression) extends BinaryOperator
+case class Divide(left: Expression, right: Expression) extends BinaryOperator
+
+trait UnaryOperation extends Expression {
+  val expr: Expression
+  override def isLiteral = expr.isLiteral
+  def gatherFields = expr.gatherFields
+}
+case class Not(expr: Expression) extends UnaryOperation
+case class UnaryPlus(expr: Expression) extends UnaryOperation
+case class UnaryMinus(expr: Expression) extends UnaryOperation
+case class Exists(select: SelectStatement) extends Expression {
+  def gatherFields = Seq.empty
+}
+
+case class FieldIdent(qualifier: Option[String], name: String, symbol: Symbol = null) extends Expression {
+  def gatherFields = Seq((this, false))
+}
+
+trait Aggregation extends Expression
+case class CountAll() extends Aggregation {
+  def gatherFields = Seq.empty
+}
+case class CountExpr(expr: Expression) extends Aggregation {
+  def gatherFields = expr.gatherFields.map(_.copy(_2 = true))
+}
+case class Sum(expr: Expression) extends Aggregation {
+  def gatherFields = expr.gatherFields.map(_.copy(_2 = true))
+}
+case class Avg(expr: Expression) extends Aggregation {
+  def gatherFields = expr.gatherFields.map(_.copy(_2 = true))
+}
+case class Min(expr: Expression) extends Aggregation {
+  def gatherFields = expr.gatherFields.map(_.copy(_2 = true))
+}
+case class Max(expr: Expression) extends Aggregation {
+  def gatherFields = expr.gatherFields.map(_.copy(_2 = true))
+}
+
+trait LiteralExpr extends Expression {
+  override def isLiteral = true
+  def gatherFields = Seq.empty
+}
+case class IntLiteral(v: Long) extends LiteralExpr
+case class FloatLiteral(v: Double) extends LiteralExpr
+case class StringLiteral(v: String) extends LiteralExpr
+case class NullLiteral() extends LiteralExpr
+case class DateLiteral(d: String) extends LiteralExpr
+
+trait Relation extends Node
+case class Table(name: String, alias: Option[String]) extends Relation
+case class Subquery(subquery: SelectStatement, alias: String) extends Relation
+
+sealed abstract trait JoinType
+case object InnerJoin extends JoinType
+case object LeftOuterJoin extends JoinType
+case object RightOuterJoin extends JoinType
+case object FullOuterJoin extends JoinType
+
+case class Join(left: Relation, right: Relation, tpe: JoinType, clause: Expression) extends Relation
+
+sealed abstract trait OrderType
+case object ASC extends OrderType
+case object DESC extends OrderType
+
+case class GroupBy(keys: Seq[Expression], having: Option[Expression]) extends Node
+case class OrderBy(keys: Seq[(Expression, OrderType)]) extends Node
--- lego/src/main/scala/utils/parser/Parser.scala
+++ lego/src/main/scala/utils/parser/Parser.scala
@@ -5,43 +5,157 @@ package parser
 
 import scala.util.parsing.combinator.RegexParsers
 
-object Parser extends RegexParsers {
+/**
+ * A simple SQL parser. 
+ * Based on: https://github.com/stephentu/scala-sql-parser
+ */
+object Parser extends StandardTokenParsers {
+
+    def parse(statement : String): Option[SelectStmt] = {
+    phrase(parseSelectStatement)(new lexical.Scanner(sql)) match {
+      case Success(r, q) => Option(r)
+      case _ => None
+    }
+  }
+
     def parseSelectStatement: Parser[SelectStatement] = (
-        "SELECT" ~ parseSelectList ~ "FROM" ~ parseTableExpression ~ ";".? ^^ { case _ ~ sl ~ _ ~ te => SelectStatement(sl, te) }
+        "SELECT" ~> parseProjections ~ "FROM" ~ parseRelations ~ parseWhere.? ~ parseGroupBy.? ~ parseOrderBy.? <~ ";".? ^^ { case pro ~ _ ~ tab ~ whe ~ grp ~ ord => SelectStatement(pro, tab, whe, grp, ord) }
     )
 
-    def parseSelectList: Parser[SelectList] = (
-        "*" ^^ { case _ => AllColumns() }
-        | rep1sep(parseValueExpression, ",") ^^ { case lst => ColumnList(lst) }
+    def parseProjections: Parser[Projections] = (
+        "*" ^^^ AllColumns()
+        | rep1sep(parseAliasedExpression, ",") ^^ { case lst => ExpressionProjections(lst) }
     )
 
-    // TODO: parseValueExpression
-
-    def parseTableExpression: Parser[TableExpression] = (
-        parseFromClause
-        ~ ("WHERE" ~> parseWhereClause).?
-        ~ ("GROUP BY" ~> parseGroupClause).?
-        ~ ("ORDER BY" ~> parseOrderClause).? ^^
-        { case tr ~ wh ~ grp ~ ord => TableExpression(tr, wh, grp, ord) }
+    def parseAliasedExpression: Parser[(Expression, Option[String])] = (
+        parseExpression ~ ("AS" ~> ident).? ^^ { case expr ~ alias => (expr, alias)}
     )
 
-    def parseFromClause: Parser[FromClause] = (
-        rep1sep(parseTableReference ~ , ",") ^^ { case lst => FromClause(lst) }
+    def parseExpression: Parser[Expression] = parseOr
+
+    def parseOr: Parser[Expression] =
+        parseAnd * ( "OR" ^^^ { (a: Expression, b: Expression) => Or(a, b) } )
+
+    def parseAnd: Parser[Expression] =
+        parseSimpleExpression * ( "AND" ^^^ { (a: Expression, b: Expression) => And(a, b) } )
+
+    def parseSimpleExpression: Parser[Expression] = (
+        parseAddition ~ rep(
+          ("=" | "<>" | "!=" | "<" | "<=" | ">" | ">=") ~ parseAddition ^^ {
+            case op ~ right => (op, right)
+          }
+          | "BETWEEN" ~ parseAddition ~ "AND" ~ parseAddition ^^ {
+            case op ~ a ~ _ ~ b => (op, a, b)
+          }
+          | "NOT IN" ~ "(" ~ (parseSelectStatement | rep1sep(parseExpression, ",")) ~ ")" ^^ {
+            case op ~ _ ~ a ~ _ => ("IN", a, True)
+          }
+          | "IN" ~ "(" ~ (parseSelectStatement | rep1sep(parseExpression, ",")) ~ ")" ^^ {
+            case op ~ _ ~ a ~ _ => (op, a, False)
+          }
+          | "NOT LIKE" ~ parseAddition ^^ { case op ~ a => ("LIKE", a, True) }
+          | "LIKE" ~ parseAddition ^^ { case op ~ a => (op, a, False) }
+        ) ^^ {
+          case left ~ elems =>
+            elems.foldLeft(left) {
+              case (acc, (("=", right: Expression))) => Equals(acc, right)
+              case (acc, (("<>", right: Expression))) => NotEquals(acc, right)
+              case (acc, (("!=", right: Expression))) => NotEquals(acc, right)
+              case (acc, (("<", right: Expression))) => LessThan(acc, right)
+              case (acc, (("<=", right: Expression))) => LessOrEqual(acc, right)
+              case (acc, ((">", right: Expression))) => GreaterThan(acc, right)
+              case (acc, ((">=", right: Expression))) => GreaterOrEqual(acc, right)
+              case (acc, (("BETWEEN", l: Expression, r: Expression))) => And(GreaterOrEqual(acc, l), LessOrEqual(acc, r))
+              case (acc, (("IN", e: Seq[_], n: Boolean))) => In(acc, e.asInstanceOf[Seq[Expression]], n)
+              case (acc, (("IN", s: SelectStatement, n: Boolean))) => In(acc, Seq(s), n)
+              case (acc, (("LIKE", e: Expression, n: Boolean))) => Like(acc, e, n)
+            }
+        } |
+        "NOT" ~> parseSimpleExpression ^^ (Not(_)) |
+        "EXISTS" ~> "(" ~> parseSelectStatement <~ ")" ^^ { case s => Exists(s) }
     )
 
-    def parseTableReference: Parser[TableReference] = (
-        name ^^ { case name => PrimaryTable(name) }
+    def parseAddition: Parser[Expression] =
+        parseMultiplication * (
+          "+" ^^^ { (a: Expression, b: Expression) => Add(a, b) } |
+          "-" ^^^ { (a: Expression, b: Expression) => Subtract(a, b) } )
+
+    def mult_expr: Parser[Expression] =
+        primary_expr * (
+          "*" ^^^ { (a: Expression, b: Expression) => Multiply(a, b) } |
+          "/" ^^^ { (a: Expression, b: Expression) => Divide(a, b) } )
+
+    def parsePrimaryExpression: Parser[SqlExpr] = (
+        parseLiteral |
+        parseKnownFunction |
+        ident ~ opt( "." ~> ident  | "(" ~> repsep(parseExpression, ",") <~ ")" ) ^^ {
+          case id ~ None => FieldIdent(None, id)
+          case a ~ Some( b: String ) => FieldIdent(Some(a), b)
+        } |
+        "(" ~> (parseExpression | parseSelectStatement ^^ (_)) <~ ")" |
+        "+" ~> parsePrimaryExpression ^^ (UnaryPlus(_)) |
+        "-" ~> parsePrimaryExpression ^^ (UnaryMinus(_)) |
     )
 
-    // TODO: parseWhereClause
-
-    // TODO: parseGroupClause
-
-    def parseOrderClause: Parser[OrderClause] = (
-        rep1sep(parseOrderKey ~ , ",") ^^ { case keys => OrderClause(keys) }
+    def parseKnownFunction: Parser[Expression] = (
+        "COUNT(" ~> ( "*" ^^^ CountAll() | parseExpression ^^ { case expr => CountExpr(e) }) <~ ")"
+        | "MIN(" ~> parseExpression <~ ")" ^^ (Min(_))
+        | "MAX(" ~> parseExpression <~ ")" ^^ (Max(_))
+        | "SUM(" ~>  parseExpression <~ ")" ^^ { Sum(_) }
+        | "AVG(" ~> parseExpression <~ ")" ^^ { Avg(_) }
     )
 
-    def parseOrderKey: Parser[OrderKey] = (
-        parseValueExpression <~ "ASC".? ^^ { case v => AscendingOrderKey(v) }
-        | parseValueExpression <~ "DESC" ^^ { case v => DescendingOrderKey(v) }
+    // TODO
+    def parseLiteral: Parser[Expression] = (
+        numericLit ^^ { case i => IntLiteral(i.toInt) } |
+        floatLit ^^ { case f => FloatLiteral(f.toDouble) } |
+        stringLit ^^ { case s => StringLiteral(s) } |
+        "null" ^^ (_ => NullLiteral()) |
+        "date" ~> stringLit ^^ (DateLiteral(_)) |
+          case d ~ u => IntervalLiteral(d, u)
+        }
+    )
+
+    def parseRelations: Parser[Seq[Relation]] = rep1sep(parseRelation, ",")
+
+    def parseRelation: Parser[Relation] = (
+        parseSimpleRelation ~ rep(opt(parseJoinType) ~ "JOIN" ~ parseSimpleRelation ~ "ON" ~ parseExpression ^^
+      { case tpe ~ _ ~ r ~ _ ~ e => (tpe.getOrElse(InnerJoin), r, e)}) ^^ {
+      case r ~ elems => elems.foldLeft(r) { case (x, r) => Join(x, r._2, r._1, r._3) }
+        }
+    )
+
+    def parseSimpleRelation: Parser[SqlRelation] =
+        ident ~ ("AS".? ~> ident.?) ^^ {
+          case tbl ~ alias => Table(tbl, alias)
+        } 
+        | ("(" ~> parseSelectStatement <~ ")") ~ ("AS".? ~> ident) ^^ {
+        case subq ~ alias => Subquery(subq, alias)
+    }
+
+    def parseJoinType: Parser[JoinType] = (
+        "LEFT" <~ "OUTER".? | "RIGHT" <~ "OUTER".? | "FULL OUTER") ^^ {
+          case "LEFT" => LeftOuterJoin()
+          case "RIGHT" => RightOuterJoin()
+          case "FULL OUTER" => FullOuterJoin()
+        }
+        | "INNER" ^^^ InnerJoin()
+    )
+
+
+    def parseWhere: Parser[Expression] = (
+        "WHERE" ~> expr
+    )
+
+    def parseGroupBy: Parser[GroupBy] = (
+        "GROUP BY" rep1sep(parseExpression, ",") ~ ("HAVING" ~> parseExpression).? ^^
+        { case exp ~ hav => GroupBy(exp, hav) }
+    )
+    def parseOrderBy: Parser[OrderClause] = (
+        "ORDER BY" ~> rep1sep(parseOrderKey ~ , ",") ^^ { case keys => OrderBy(keys) }
+    )
+
+    def parseOrderKey: Parser[(Expression, OrderType)] = (
+        parseExpression <~ "ASC".? ^^ { case v => (v, ASC) }
+        | parseExpression <~ "DESC" ^^ { case v => (v, DESC) }
     )
