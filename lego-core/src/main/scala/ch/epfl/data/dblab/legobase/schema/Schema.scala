fd095ee9b719b3f022e0bedeca4961647c75f453
yannisklonatos <yannis.klonatos@epfl.ch>
2015-05-17 12:52:42 +0200

    Yannis: Generalized String compression transformer (TPC-H agnostic) and fixed 4 bugs on it

--- lego-core/src/main/scala/ch/epfl/data/dblab/legobase/schema/Schema.scala
+++ lego-core/src/main/scala/ch/epfl/data/dblab/legobase/schema/Schema.scala
@@ -6,7 +6,10 @@ import sc.pardis.types._
 import scala.language.implicitConversions
 
 case class Catalog(schemata: Map[String, Schema])
-case class Schema(tables: List[Table])
+case class Schema(tables: List[Table]) {
+  def findTable(name: String) = tables.find(t => t.name == name)
+  def findAttribute(attrName: String): Option[Attribute] = tables.map(t => t.attributes).flatten.find(attr => attr.name == attrName)
+}
 case class Table(name: String, attributes: List[Attribute], constraints: List[Constraint], resourceLocator: String, var rowCount: Long) {
   def primaryKey: Option[PrimaryKey] = constraints.collectFirst { case pk: PrimaryKey => pk }
   def foreignKeys: List[ForeignKey] = constraints.collect { case fk: ForeignKey => fk }
@@ -14,7 +17,9 @@ case class Table(name: String, attributes: List[Attribute], constraints: List[Co
   def uniques: List[Unique] = constraints.collect { case unq: Unique => unq }
   def autoIncrement: Option[AutoIncrement] = constraints.collectFirst { case ainc: AutoIncrement => ainc }
 }
-case class Attribute(name: String, dataType: Tpe, var distinctValuesCount: Int = 0, var nullValuesCount: Long = 0)
+case class Attribute(name: String, dataType: Tpe, constraints: List[Constraint] = List(), var distinctValuesCount: Int = 0, var nullValuesCount: Long = 0) {
+  def hasConstraint(con: Constraint) = constraints.contains(con)
+}
 object Attribute {
   implicit def tuple2ToAttribute(nameAndType: (String, Tpe)): Attribute = Attribute(nameAndType._1, nameAndType._2)
 }
@@ -29,5 +34,6 @@ case class ForeignKey(ownTable: String, referencedTable: String, attributes: Lis
 case class NotNull(attribute: Attribute) extends Constraint
 case class Unique(attribute: Attribute) extends Constraint
 case class AutoIncrement(attribute: Attribute) extends Constraint
+object Compressed extends Constraint
 
 // TODO add a type representation for fixed-size string
