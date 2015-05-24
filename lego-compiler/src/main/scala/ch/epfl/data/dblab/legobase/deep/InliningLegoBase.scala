c0b2abfe4e8cfa20665980632cdc7b6f0e2a2761
Amir Shaikhha <amir.shaikhha@epfl.ch>
2015-05-24 14:09:43 +0200

    Partially evaluating null check for constructors

--- lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/deep/InliningLegoBase.scala
+++ lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/deep/InliningLegoBase.scala
@@ -9,7 +9,19 @@ import sc.pardis.types.PardisTypeImplicits._
 // TODO volcano cakes should be added whenever we have support for both volcano and push engines
 // trait InliningLegoBase extends volcano.InliningVolcano with DeepDSL with sc.pardis.ir.InlineFunctions with tpch.QueriesImplementations with LoopUnrolling with schema.InliningLoader
 /** A polymorphic embedding cake which chains all cakes responsible for further partial evaluation. */
-trait InliningLegoBase extends queryengine.push.InliningPush with DeepDSL with sc.pardis.ir.InlineFunctions with tpch.QueriesImplementations with LoopUnrolling with storagemanager.InliningLoader with tpch.InliningTPCHLoader
+trait InliningLegoBase extends queryengine.push.InliningPush with DeepDSL with sc.pardis.ir.InlineFunctions with tpch.QueriesImplementations with LoopUnrolling with storagemanager.InliningLoader with tpch.InliningTPCHLoader {
+  override def infix_!=[A: TypeRep, B: TypeRep](a: Rep[A], b: Rep[B]): Rep[Boolean] = (a, b) match {
+    case (Def(d: ConstructorDef[_]), Constant(null)) => unit(true)
+    case (Constant(null), Def(d: ConstructorDef[_])) => unit(true)
+    case _ => super.infix_!=(a, b)
+  }
+
+  override def infix_==[A: TypeRep, B: TypeRep](a: Rep[A], b: Rep[B]): Rep[Boolean] = (a, b) match {
+    case (Def(d: ConstructorDef[_]), Constant(null)) => unit(false)
+    case (Constant(null), Def(d: ConstructorDef[_])) => unit(false)
+    case _ => super.infix_==(a, b)
+  }
+}
 
 /** A polymorphic embedding cake for performing loop unrolling in the case of using var args. */
 trait LoopUnrolling extends sc.pardis.ir.InlineFunctions { this: InliningLegoBase =>
