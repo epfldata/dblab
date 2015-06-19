54fa6f44d636e8e41653122a10e1419ae360cfff
Amir Shaikhha <amir.shaikhha@epfl.ch>
2015-06-19 21:18:10 +0200

    Quasi traits parents are more appropriate now

--- lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/quasi/LegoBaseQuasi.scala
+++ lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/quasi/LegoBaseQuasi.scala
@@ -3,12 +3,21 @@ package dblab.legobase
 package quasi
 
 import deep.LoweringLegoBase
-import sc.pardis.deep.{ DSLExpOps, DSLExtOps }
 import sc.pardis.deep.scalalib._
 import sc.pardis.deep.scalalib.collection._
+import sc.pardis.quasi.anf.{ BaseYY, BaseExt }
 
-trait LegoBaseQuasiExp extends LoweringLegoBase with DSLExpOps {
+trait LegoBaseQuasiExp extends LoweringLegoBase
+  with BaseYY
 
-}
-
-trait LegoBaseQuasiExt extends DSLExtOps with ArrayExtOps with RangeExtOps with BooleanExtOps
+trait LegoBaseQuasiExt extends BaseExt
+  with NumericExtOps
+  with ByteExtOps
+  with SetExtOps
+  with PairExtOps
+  with ArrayBufferExtOps
+  with ArrayExtOps
+  with RangeExtOps
+  with BooleanExtOps
+  with MultiMapExtOps
+  with OptionExtOps
