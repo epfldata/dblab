f030814928650a60581fd1ee2f0c9bb5caa7b903
Amir Shaikhha <amir.shaikhha@epfl.ch>
2015-06-24 19:10:10 +0200

    Refactored and cleaned up WhileToRangeForeachTransformer

--- lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/optimization/WhileToRangeForeachTransformer.scala
+++ lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/optimization/WhileToRangeForeachTransformer.scala
@@ -18,21 +18,58 @@ import quasi._
  * @param IR the polymorphic embedding trait which contains the reified program.
  */
 class WhileToRangeForeachTransformer(override val IR: LoweringLegoBase) extends RuleBasedTransformer[LoweringLegoBase](IR) {
-  import IR._
+  import IR.{ Range => _, _ }
 
+  /**
+   * Keeps the information about a while loop
+   */
+  case class WhileInfo(whileSym: Rep[Unit],
+                       whileNode: While,
+                       variable: Var[Int],
+                       size: Rep[Int],
+                       step: Rep[Int]) {
+    def start: Rep[Int] = variable match {
+      case Var(Def(NewVar(init))) => init
+      case _ =>
+        throw new Exception(s"No initial value can be computed for the" +
+          s" while loop with the following info:\n$this")
+    }
+  }
+
+  /**
+   * Keeps the list of while loops that should be converted
+   */
+  val convertedWhiles = scala.collection.mutable.ArrayBuffer[WhileInfo]()
+  /**
+   * Specifies that we are in the transformed while loop body and the iterating variable
+   * of the while loop should be substituted by the index of foreach which is given
+   */
+  val substituteVarInsideLoopBody = scala.collection.mutable.Map[Var[Any], Rep[Int]]()
+
+  def whileShouldBeConverted[T](whileSym: Rep[T]): Boolean =
+    convertedWhiles.exists(_.whileSym == whileSym)
+
+  def varShouldBeRemoved[T](variable: Var[T]): Boolean =
+    convertedWhiles.exists(_.variable == variable)
+
+  /**
+   * Extracts the condition part of a while loop
+   */
   object RangeCondition {
     def unapply(block: Block[Boolean]): Option[(Var[Int], Rep[Int])] = {
       val resultNode = block.stmts.find(stm => stm.sym == block.res).get.rhs
       resultNode match {
-        case dsl"true && $block2"             => RangeCondition.unapply(block2)
+        case dsl"true && $block2" => RangeCondition.unapply(block2)
         // TODO needs having readVar in shallow or some other mechanism to detect vars
-        // case dsl"(${ Def(ReadVar(v)) }: Int) < $size" => Some(v -> size)
-        case Int$less1(Def(ReadVar(v)), size) => Some(v -> size)
-        case _                                => None
+        case dsl"(${ Def(ReadVar(v)) }: Int) < ($size: Int)" => Some(v -> size)
+        case _ => None
       }
     }
   }
 
+  /**
+   * Extracts the stepping part of a while loop
+   */
   object RangeStep {
     def unapply(block: Block[Unit]): Option[(Var[Int], Rep[Int])] = block.stmts.last.rhs match {
       case Assign(v, Def(Int$plus2(Def(ReadVar(v2)), step))) if v == v2 => Some(v2 -> step)
@@ -40,39 +77,21 @@ class WhileToRangeForeachTransformer(override val IR: LoweringLegoBase) extends 
     }
   }
 
-  // def isConditionForCheckingSize(block: Block[Boolean]): Boolean = {
-  //   val resultNode = block.stmts.find(stm => stm.sym == block.res).get.rhs
-  //   resultNode match {
-  //     case Boolean$amp$amp(Constant(true), block2) => isConditionForCheckingSize(block2)
-  //     case Int$less3(Def(ReadVar(v)), size)        => true
-  //     case _                                       => false
-  //   }
-  // }
-
-  case class WhileInfo(whileSym: Rep[Unit], whileNode: While, variable: Var[Int], size: Rep[Int], step: Rep[Int])
-
-  val convertedWhiles = scala.collection.mutable.ArrayBuffer[WhileInfo]()
-  val startConds = scala.collection.mutable.Map[Var[Int], Rep[Int]]()
-  val fillingPhase = scala.collection.mutable.Map[Var[Any], Rep[Int]]()
-  def shouldBeConverted[T](whileSym: Rep[T]): Boolean = convertedWhiles.exists(_.whileSym == whileSym)
-  def varShouldBeRemoved[T](variable: Var[T]): Boolean = convertedWhiles.exists(_.variable == variable)
-
   analysis += statement {
-    case sym -> (node @ dsl"while(${ RangeCondition(variable1, size) }) ${ RangeStep(variable2, step) }") if variable1 == variable2 =>
-      // val isForEach = isConditionForCheckingSize(cond)
+    case sym -> (node @ dsl"""while(${ RangeCondition(variable1, size) }) 
+                                ${ RangeStep(variable2, step) }""") if variable1 == variable2 =>
       convertedWhiles += WhileInfo(sym.asInstanceOf[Rep[Unit]], node.asInstanceOf[While], variable1, size, step)
-      // System.out.println(s"$sym -> $variable1, $size")
       ()
   }
 
   rewrite += remove {
-    case ReadVar(v) if varShouldBeRemoved(v) && !fillingPhase.contains(v) =>
+    case ReadVar(v) if varShouldBeRemoved(v) && !substituteVarInsideLoopBody.contains(v) =>
       ()
   }
 
   rewrite += rule {
-    case ReadVar(v) if varShouldBeRemoved(v) && fillingPhase.contains(v) =>
-      fillingPhase(v)
+    case ReadVar(v) if varShouldBeRemoved(v) && substituteVarInsideLoopBody.contains(v) =>
+      substituteVarInsideLoopBody(v)
   }
 
   rewrite += remove {
@@ -80,37 +99,20 @@ class WhileToRangeForeachTransformer(override val IR: LoweringLegoBase) extends 
       ()
   }
 
-  analysis += statement {
-    case sym -> NewVar(e) =>
-      startConds += Var(sym.asInstanceOf[Rep[Var[Int]]]) -> e.asInstanceOf[Rep[Int]]
-      ()
-  }
-
-  // rewrite += remove {
-  //   case node @ NewVar(e) if convertedWhiles.exists(_.variable.e.correspondingNode == node) =>
-  //     // startConds += Var(sym.asInstanceOf[Rep[Var[Int]]]) -> e.asInstanceOf[Rep[Int]]
-  //     // val flag = varShouldBeRemoved(Var(sym.asInstanceOf[Rep[Var[Any]]]))
-  //     System.out.println(s"var sym: $node -> ")
-  //     ()
-  // }
-
   rewrite += statement {
-    case sym -> dsl"while($cond) $body" if shouldBeConverted(sym) =>
+    case sym -> dsl"while($cond) $body" if whileShouldBeConverted(sym) =>
       val whileInfo = convertedWhiles.find(_.whileSym == sym).get
-      val start = startConds(whileInfo.variable)
 
       def foreachFunction: Rep[Int => Unit] = {
         // implicitly injects __lambda here
         (i: Rep[Int]) =>
           {
-            fillingPhase += whileInfo.variable -> i
+            substituteVarInsideLoopBody += whileInfo.variable -> i
+            // Triggers rewriting the statements inside the while loop
             body.stmts.foreach(transformStm)
             unit(())
           }
       }
-      // System.out.println("here!")
-      // reify {
-      dsl"""scala.collection.immutable.Range($start, ${whileInfo.size}).foreach($foreachFunction)"""
-    // }
+      dsl"""Range(${whileInfo.start}, ${whileInfo.size}).foreach($foreachFunction)"""
   }
 }
