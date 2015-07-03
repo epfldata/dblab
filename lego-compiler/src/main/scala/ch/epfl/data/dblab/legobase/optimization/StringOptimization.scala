87be3b27a5fe779c891008c49dc483d7e33a33f0
amirsh <amir.shaikhha@epfl.ch>
2015-07-03 13:40:57 +0200

    Example and documentation added for StringOptimization

--- lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/optimization/StringOptimization.scala
+++ lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/optimization/StringOptimization.scala
@@ -11,9 +11,22 @@ import sc.pardis.types._
 import sc.pardis.types.PardisTypeImplicits._
 import sc.pardis.shallow.utils.DefaultValue
 
+/**
+ * Fine-grained optimizations for string operations.
+ *
+ * Example:
+ * {{{
+ *    string.startsWith("foo")
+ * }}}
+ * is converted to
+ * {{{
+ *    string(0) == 'f' && string(1) == 'o' && string(2) == 'o'
+ * }}}
+ */
 class StringOptimization(override val IR: LoweringLegoBase) extends RecursiveRuleBasedTransformer[LoweringLegoBase](IR) {
   import IR._
 
+  // TODO needs quasi lifting legobase.
   rewrite += rule {
     case OptimalStringStartsWith(str, Def(GenericEngineParseStringObject(Constant(str2: String)))) =>
       str2.toCharArray.zipWithIndex.foldLeft(unit(true))((acc, curr) => acc && (str(curr._2) __== unit(curr._1)))
