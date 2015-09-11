4ea3d48d773f901c638ccd0a16bbb0ff559da222
Amir Shaikhha <amir.shaikhha@epfl.ch>
2015-09-11 02:38:37 -1000

    printRows method inlined in lego compiler.

--- lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/deep/InliningLegoBase.scala
+++ lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/deep/InliningLegoBase.scala
@@ -9,7 +9,10 @@ import sc.pardis.types.PardisTypeImplicits._
 // TODO volcano cakes should be added whenever we have support for both volcano and push engines
 // trait InliningLegoBase extends volcano.InliningVolcano with DeepDSL with sc.pardis.ir.InlineFunctions with tpch.QueriesImplementations with LoopUnrolling with schema.InliningLoader
 /** A polymorphic embedding cake which chains all cakes responsible for further partial evaluation. */
-trait InliningLegoBase extends queryengine.push.InliningPush with DeepDSL with sc.pardis.ir.InlineFunctions with tpch.QueriesImplementations with LoopUnrolling with storagemanager.InliningLoader with tpch.InliningTPCHLoader {
+trait InliningLegoBase extends queryengine.push.InliningPush with DeepDSL
+  with sc.pardis.ir.InlineFunctions with tpch.QueriesImplementations
+  with LoopUnrolling with storagemanager.InliningLoader
+  with tpch.InliningTPCHLoader with queryengine.monad.QueryInline {
   override def infix_!=[A: TypeRep, B: TypeRep](a: Rep[A], b: Rep[B]): Rep[Boolean] = (a, b) match {
     case (Def(d: ConstructorDef[_]), Constant(null)) => unit(true)
     case (Constant(null), Def(d: ConstructorDef[_])) => unit(true)
