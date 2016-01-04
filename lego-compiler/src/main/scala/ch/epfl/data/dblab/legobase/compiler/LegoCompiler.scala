3e26fdce159196b7bbe332c2c28d52897f15ab8b
amirsh <amir.shaikhha@epfl.ch>
2016-01-04 22:50:56 +0100

    flags for monad-iterator lowering added.

--- lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/compiler/LegoCompiler.scala
+++ lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/compiler/LegoCompiler.scala
@@ -84,6 +84,8 @@ class LegoCompiler(val DSL: LegoBaseExp,
     pipeline += DCE
     if (settings.queryMonadCPS) {
       pipeline += new QueryMonadCPSLowering(schema, DSL)
+    } else if (settings.queryMonadIterator) {
+      // pipeline += new QueryMonadIteratorLowering(schema, DSL)
     } else {
       pipeline += new QueryMonadLowering(schema, DSL)
     }
