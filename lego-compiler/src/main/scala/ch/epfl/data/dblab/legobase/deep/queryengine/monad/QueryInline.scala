4a574d2f82f0983b4ecb9699cbceeb492609813d
amirsh <amir.shaikhha@epfl.ch>
2016-01-05 18:24:37 +0100

    Partially evaluating pair field accesses.

--- lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/deep/queryengine/monad/QueryInline.scala
+++ lego-compiler/src/main/scala/ch/epfl/data/dblab/legobase/deep/queryengine/monad/QueryInline.scala
@@ -24,4 +24,13 @@ trait QueryInline extends QueryOps with sc.pardis.deep.scalalib.ScalaPredefOps {
     // res2: Seq[Manifest[Any]] = WrappedArray(Any, Any)
     printf(unit("(%d rows)\n"), readVar[Int](rows))
   }
+
+  override def tuple2_Field__2[T1, T2](self: Rep[Tuple2[T1, T2]])(implicit typeT1: TypeRep[T1], typeT2: TypeRep[T2]): Rep[T2] = self match {
+    case Def(node: Tuple2ApplyObject[_, _]) => node._2
+    case _                                  => super.tuple2_Field__2[T1, T2](self)(typeT1, typeT2)
+  }
+  override def tuple2_Field__1[T1, T2](self: Rep[Tuple2[T1, T2]])(implicit typeT1: TypeRep[T1], typeT2: TypeRep[T2]): Rep[T1] = self match {
+    case Def(node: Tuple2ApplyObject[_, _]) => node._1
+    case _                                  => super.tuple2_Field__1[T1, T2](self)(typeT1, typeT2)
+  }
 }
