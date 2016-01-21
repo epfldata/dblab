421b7ce97e8d3d1bb650c505a0fe1b4cf2aa3ca1
amirsh <amir.shaikhha@epfl.ch>
2016-01-21 13:41:00 +0100

    minor change in synthetic query

--- lego-compiler/src/test/scala/SyntheticQueries.scala
+++ lego-compiler/src/test/scala/SyntheticQueries.scala
@@ -34,7 +34,7 @@ object SyntheticQueries extends TPCHRunner {
     settings = new Settings(args.toList)
     val years = 1992 to 1998
     // 1996
-    val months = 1 to 12 map (x => if (x < 10) s"0$x" else x.toString)
+    val months = (1 to 12 by 2) map (x => if (x < 10) s"0$x" else x.toString)
     val dates = for (y <- years; m <- months) yield s"$y-$m-01"
     // val days = 1 to 30 map (x => if (x < 10) s"0$x" else x.toString)
     // val dates = for (d <- days) yield s"1996-12-$d"
