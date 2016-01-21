4460d881d1efe572be3a994dfcf55e55e70d968f
amirsh <amir.shaikhha@epfl.ch>
2016-01-21 15:03:05 +0100

    An option added for using a single loader or separate loader for synthetic queries

--- lego-compiler/src/test/scala/SyntheticQueries.scala
+++ lego-compiler/src/test/scala/SyntheticQueries.scala
@@ -23,6 +23,21 @@ object SyntheticQueries extends TPCHRunner {
 
   var settings: Settings = _
 
+  val ONE_LOADER_FOR_ALL = true
+  val oneAgg = false
+  // val oneAgg = true
+
+  def datesGenerator: List[String] = {
+    val years = 1992 to 1998
+    // 1996
+    val months = (1 to 12 by 2) map (x => if (x < 10) s"0$x" else x.toString)
+    val dates = for (y <- years; m <- months) yield s"$y-$m-01"
+    // val days = 1 to 30 map (x => if (x < 10) s"0$x" else x.toString)
+    // val dates = for (d <- days) yield s"1996-12-$d"
+    // val dates = List("1998-01-01")
+    dates.toList
+  }
+
   var param: String = _
 
   def main(args: Array[String]) {
@@ -32,18 +47,15 @@ object SyntheticQueries extends TPCHRunner {
     }
     Config.checkResults = false
     settings = new Settings(args.toList)
-    val years = 1992 to 1998
-    // 1996
-    val months = (1 to 12 by 2) map (x => if (x < 10) s"0$x" else x.toString)
-    val dates = for (y <- years; m <- months) yield s"$y-$m-01"
-    // val days = 1 to 30 map (x => if (x < 10) s"0$x" else x.toString)
-    // val dates = for (d <- days) yield s"1996-12-$d"
-    // val dates = List("1998-01-01")
-    for (d <- dates) {
-      param = d
-      run(args)
-    }
 
+    if (ONE_LOADER_FOR_ALL) {
+      run(args)
+    } else {
+      for (d <- datesGenerator) {
+        param = d
+        run(args)
+      }
+    }
   }
 
   implicit val context = new LegoBaseExp {}
@@ -53,70 +65,68 @@ object SyntheticQueries extends TPCHRunner {
     import dblab.legobase.tpch.TPCHLoader._
     import dblab.legobase.queryengine.GenericEngine._
     val lineitemTable = dsl"""Query(loadLineitem())"""
-    // val years = 1992 to 1998 // 1996
-    // val months = 1 to 12 map (x => if (x < 10) s"0$x" else x.toString)
-    // val dates1 = for (y <- years; m <- months) yield s"$y-$m-01"
-    // val days = 2 to 30 map (x => if (x < 10) s"0$x" else x.toString)
-    // val dates2 = for (d <- days) yield s"1996-12-$d"
-    // val dates = dates1 ++ dates2
-    // val dates = dates1
-    // for (startDate <- dates) {
-    val startDate = param
-    def selection = dsl"""
-      val constantDate1: Int = parseDate($startDate)
-      val constantDate2: Int = parseDate("1997-01-01")
-      $lineitemTable
-        //.filter(x => x.L_SHIPDATE >= constantDate1 && (x.L_SHIPDATE < constantDate2 && (x.L_DISCOUNT >= 0.08 && (x.L_DISCOUNT <= 0.1 && (x.L_QUANTITY < 24))))) 
-        .filter(x => x.L_SHIPDATE >= constantDate1)
-      """
-    def selectivity = dsl"""
-        val filtered = $selection
-        val original = $lineitemTable
-        filtered.count.asInstanceOf[Double] / original.count
-      """
-    val oneAgg = false
-    // val oneAgg = true
-    dsl"""
-      printf($startDate)
-      printf("\nselectivity: %f\n", $selectivity)
-      printf("==========\n")"""
-    if (oneAgg)
+
+    def generateQueryForStartDate(startDate: String): context.Rep[Unit] = {
+      def selection = dsl"""
+        val constantDate1: Int = parseDate($startDate)
+        val constantDate2: Int = parseDate("1997-01-01")
+        $lineitemTable
+          //.filter(x => x.L_SHIPDATE >= constantDate1 && (x.L_SHIPDATE < constantDate2 && (x.L_DISCOUNT >= 0.08 && (x.L_DISCOUNT <= 0.1 && (x.L_QUANTITY < 24))))) 
+          .filter(x => x.L_SHIPDATE >= constantDate1)
+        """
+      def selectivity = dsl"""
+          val filtered = $selection
+          val original = $lineitemTable
+          filtered.count.asInstanceOf[Double] / original.count
+        """
       dsl"""
-        runQuery {
-          val result = {
+        printf($startDate)
+        printf("\nselectivity: %f\n", $selectivity)
+        printf("==========\n")"""
+      if (oneAgg)
+        dsl"""
+          runQuery {
+            val result = {
+              val filtered = $selection
+              var sumResult = 0.0
+              for(t <- filtered) {
+                sumResult += t.L_EXTENDEDPRICE * t.L_DISCOUNT
+              }
+              sumResult
+            }
+            printf("%.4f\n", result)
+          }
+        printf("==========\n")
+        """
+      else
+        dsl"""
+          runQuery {
             val filtered = $selection
             var sumResult = 0.0
+            var sumResult2 = 0.0
+            var sumResult3 = 0.0
             for(t <- filtered) {
               sumResult += t.L_EXTENDEDPRICE * t.L_DISCOUNT
+              sumResult2 += t.L_DISCOUNT
+              sumResult3 += t.L_EXTENDEDPRICE
             }
-            sumResult
+            val r1 = sumResult
+            val r2 = sumResult2
+            val r3 = sumResult3
+            printf("%.4f, %.4f, %.4f\n", r1, r2, r3)
           }
-          printf("%.4f\n", result)
-        }
-      printf("==========\n")
-      """
-    else
-      dsl"""
-        runQuery {
-          val filtered = $selection
-          var sumResult = 0.0
-          var sumResult2 = 0.0
-          var sumResult3 = 0.0
-          for(t <- filtered) {
-            sumResult += t.L_EXTENDEDPRICE * t.L_DISCOUNT
-            sumResult2 += t.L_DISCOUNT
-            sumResult3 += t.L_EXTENDEDPRICE
-          }
-          val r1 = sumResult
-          val r2 = sumResult2
-          val r3 = sumResult3
-          printf("%.4f, %.4f, %.4f\n", r1, r2, r3)
-        }
-      printf("==========\n")
-      """
+        printf("==========\n")
+        """
+    }
 
-    // }
-    context.unit(())
+    if (ONE_LOADER_FOR_ALL) {
+      for (d <- datesGenerator) {
+        generateQueryForStartDate(d)
+      }
+      context.unit()
+    } else {
+      generateQueryForStartDate(param)
+    }
   }
 
   def executeQuery(query: String, schema: Schema): Unit = {
@@ -128,7 +138,12 @@ object SyntheticQueries extends TPCHRunner {
     val validatedSettings = settings.validate()
 
     val compiler = new LegoCompiler(context, validatedSettings, schema, "ch.epfl.data.dblab.legobase.tpch.TPCHRunner") {
-      override def outputFile = s"${param}_${super.outputFile}"
+      def postfix: String = s"sf${settings.args(1)}_${super.outputFile}"
+      override def outputFile =
+        if (ONE_LOADER_FOR_ALL)
+          postfix
+        else
+          s"${param}_${postfix}"
     }
     compiler.compile(queryFunction())
   }
