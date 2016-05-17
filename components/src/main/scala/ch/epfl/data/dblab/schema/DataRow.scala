cb9df22624033c031d13829a1e914c071919be3e
amirsh <amir.shaikhha@epfl.ch>
2016-05-17 16:29:46 +0200

    DynamicDataRow added to query interpretation * It is used for defining an anonymous record for queries. One of the main use-cases is for defining intermediate records.

--- components/src/main/scala/ch/epfl/data/dblab/schema/DataRow.scala
+++ components/src/main/scala/ch/epfl/data/dblab/schema/DataRow.scala
@@ -7,7 +7,7 @@ import sc.pardis.shallow.{ Record, OptimalString }
 /**
  * @author Yannis Klonatos
  */
-case class DataRow(values: Seq[(String, Any)]) extends Record {
+class DataRow(values: Seq[(String, Any)]) extends Record {
   private val fieldMap = collection.mutable.HashMap[String, Any](values: _*)
   def numFields = fieldMap.size
   def getField(name: String): Option[Any] = fieldMap.get(name) match {
@@ -31,4 +31,17 @@ case class DataRow(values: Seq[(String, Any)]) extends Record {
   def getFieldNames() = fieldMap.keySet
   def getNestedRecords(): Seq[DataRow] = fieldMap.map({ case (k, v) => v }).filter(_.isInstanceOf[DataRow]).toSeq.asInstanceOf[Seq[DataRow]]
   override def toString = "DataRow(" + fieldMap.toSeq.toString + ")"
+}
+
+import scala.language.dynamics
+
+class DynamicDataRow(val className: String, values: Seq[(String, Any)]) extends DataRow(values.toSeq) with Dynamic {
+  def selectDynamic[T](key: String): T = {
+    getField(key).getOrElse(sys.error(s"$this does not have $key field")).asInstanceOf[T]
+  }
+}
+
+object DynamicDataRow {
+  def apply(className: String)(values: (String, Any)*): DynamicDataRow =
+    new DynamicDataRow(className, values.toSeq)
 }
