8617fa8cc95fffbcfc99866b7ab148f41e6315d5
sajerner <sajerner@gmail.com>
2017-07-19 14:33:35 +0200

    Add CalcAST.

--- components/src/main/scala/ch/epfl/data/dblab/frontend/parser/CalcAST.scala
+++ components/src/main/scala/ch/epfl/data/dblab/frontend/parser/CalcAST.scala
@@ -6,26 +6,55 @@ package parser
 import sc.pardis.types.PardisType
 
 /**
- * A module containing AST nodes for AGCA constructs.
+ * A module containing AST nodes for AG
+ * CA constructs.
  *
  * @author Mohsen Ferdosi
  */
 object CalcAST {
   trait CalcExpr
-  case class CalcSum(exprs: List[CalcExpr]) extends CalcExpr
-  // TODO CalcProd
+  case class CalcSum(exprs: List[CalcExpr]) extends CalcExpr // Why ?
+  case class CalcProd(exprs: List[CalcExpr]) extends CalcExpr
   case class CalcNeg(expr: CalcExpr) extends CalcExpr
   case class AggSum(vars: List[VarT], expr: CalcExpr) extends CalcExpr
-  // TODO the rest
+  case class Rel(name: String, vars: List[VarT]) extends CalcExpr
+  case class External(e: CalcExpr, et: External_t) extends CalcExpr
+  case class Cmp(cmp: Cmp_t, first: ArithExpr, second: ArithExpr) extends CalcExpr
+  case class CmpOrList(v: ArithExpr, consts: List[Const_t]) extends CalcExpr
+  case class Lift(vr: VarT, expr: CalcExpr) extends CalcExpr
+  case class Exists(term: CalcExpr) extends CalcExpr
   case class CalcValue(v: ArithExpr) extends CalcExpr
 
   trait ArithExpr
   case class ArithSum(expr: List[ArithExpr]) extends ArithExpr
-  // TODO ArithProd
+  case class ArithProd(expr: List[ArithExpr]) extends ArithExpr
   case class ArithNeg(expr: ArithExpr) extends ArithExpr
   case class ArithConst(lit: SQLAST.LiteralExpression) extends ArithExpr
   case class ArithVar(v: VarT) extends ArithExpr
-  // TODO ArithFunc
+  case class ArithFunc(name: String, terms: List[ArithExpr], tp: PardisType[_]) extends ArithExpr
 
   case class VarT(name: String, tp: PardisType[_])
+
+  case class External_t(name: String, inps: List[VarT], outs: List[VarT], tp: PardisType[_], meta: CalcExpr) //TODO meta should be optional
+
+  trait Cmp_t
+  case object Eq extends Cmp_t // Equals
+  case object Lt extends Cmp_t // Less Than
+  case object Lte extends Cmp_t // Less Than or equal
+  case object Gt extends Cmp_t // Greater Than
+  case object Gte extends Cmp_t // Greater Than or equal
+  case object Neq extends Cmp_t // Not equals
+
+  trait Const_t
+  case class CBool(value: Boolean) extends Const_t
+  case class CInt(value: Int) extends Const_t
+  case class CFloat(value: Float) extends Const_t
+  case class CString(value: String) extends Const_t
+  case class CDate(y: Int, m: Int, d: Int) extends Const_t
+  case class CInterval(it: Interval_t) extends Const_t
+
+  trait Interval_t
+  case class CYearMonth(y: Int, m: Int) extends Interval_t
+  case class CDay(value: Int) extends Interval_t
+
 }
