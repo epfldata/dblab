5d080fcef8f339b2b99e656b49e3aec545bc5c22
Parand <parand1997@gmail.com>
2017-08-18 15:13:24 +0200

    testing nesting rewrites

--- components/src/test/scala/ch/epfl/data/dblab/frontend/parser/CalcOptimizerTest.scala
+++ components/src/test/scala/ch/epfl/data/dblab/frontend/parser/CalcOptimizerTest.scala
@@ -5,6 +5,7 @@ import Matchers._
 import ch.epfl.data.dblab.frontend.optimizer.CalcOptimizer
 import ch.epfl.data.dblab.frontend.parser.CalcAST._
 import ch.epfl.data.dblab.frontend.parser.SQLAST.IntLiteral
+import ch.epfl.data.sc.pardis.types.IntType
 
 /**
  * @author Parand Alizadeh
@@ -15,10 +16,42 @@ class CalcOptimizerTest extends FlatSpec {
     val optimizer = CalcOptimizer
     val r = CalcProd(List(CalcValue(ArithConst(IntLiteral(2))), CalcValue(ArithConst(IntLiteral(3))), CalcValue(ArithConst(IntLiteral(4))), CalcValue(ArithConst(IntLiteral(5))), CalcSum(List(CalcValue(ArithConst(IntLiteral(3))), CalcValue(ArithConst(IntLiteral(0))), CalcValue(ArithConst(IntLiteral(4)))))))
     val res = optimizer.Normalize(r)
-    println("#######")
+    res should not be None
+  }
+
+  "nesting rewrite" should "optimize query with zero in aggsum" in {
+
+    val optimizer = CalcOptimizer
+    val r = AggSum(List(VarT("A", IntType)), CalcValue(ArithConst(IntLiteral(0))))
+
+    val res = optimizer.nestingRewrites(r)
+    println("############")
     println(prettyprint(r))
     println(prettyprint(res))
     res should not be None
   }
 
+  "nesting rewrite" should "optimize aggsum query" in {
+
+    val optimizer = CalcOptimizer
+    val r = AggSum(List(VarT("A", IntType)), CalcValue(ArithVar(VarT("A", IntType))))
+
+    val res = optimizer.nestingRewrites(r)
+    println("############")
+    println(prettyprint(r))
+    println(prettyprint(res))
+    res should not be None
+  }
+
+  "nesting rewrite" should "optimize aggsum query with sum" in {
+
+    val optimizer = CalcOptimizer
+    val r = CalcSum(List(AggSum(List(VarT("A", IntType)), CalcValue(ArithVar(VarT("A", IntType)))), AggSum(List(VarT("B", IntType)), CalcValue(ArithVar(VarT("B", IntType)))), AggSum(List(VarT("C", IntType)), CalcValue(ArithVar(VarT("C", IntType))))))
+
+    val res = optimizer.nestingRewrites(r)
+    println("############")
+    println(prettyprint(r))
+    println(prettyprint(res))
+    res should not be None
+  }
 }
