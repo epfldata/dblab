7ebc974a8b32037f9153c97441c4fe36a826ae73
Mohsen Ferdosi <sajerner@gmail.com>
2017-11-05 17:28:15 +0330

    simple test passed before implementing lift

--- components/src/main/scala/ch/epfl/data/dblab/frontend/analyzer/CalcCosting.scala
+++ components/src/main/scala/ch/epfl/data/dblab/frontend/analyzer/CalcCosting.scala
@@ -16,58 +16,75 @@ class CalcCosting(schema: Schema) extends CostingContext {
   // most probably an appropriate cost will be parameteric and dependent on
   // the cardinality of relations.
 
-  println("here : ")
-  println(schema)
+  //  println("here : ")
+  //  println(schema)
 
   def searchCost(x: Double): Double = max(1, scala.math.log(x) / scala.math.log(2))
 
   def cost(exp: CalcExpr): Double = {
 
+    //    println(exp)
+
     val r = exp match {
       case CalcProd(List()) => 0.0
       case CalcProd(lst) =>
-        val headCardinality = cardinality(lst.head)
+        val headCardinality = cardinality(lst.head)._2
         lst.map(cost).sum +
-          lst.foldLeft((1.0, 0.0))((a, b) => (a._1 * cardinality(b), a._2 + a._1 * cardinality(b)))._2 - headCardinality
+          lst.foldLeft((1.0, 0.0))((a, b) => {
+            if (cardinality(b)._1 == 2)
+              (a._1 * cardinality(b)._2, a._2 + a._1 * cardinality(b)._2)
+            else
+              (a._1, a._2 + a._1)
+          })._2 //- headCardinality
       case CalcSum(lst) =>
-        val headCardinality = cardinality(lst.head)
-        lst.map(cost).sum + lst.map(x => searchCost(cardinality(x))).sum +
+        val headCardinality = cardinality(lst.head)._2
+        lst.map(cost).sum + lst.map(x => searchCost(cardinality(x)._2)).sum +
           headCardinality - searchCost(headCardinality) //TODO what if they had different rows ?
-      case CalcNeg(e)                     => cost(e) + cardinality(e)
-      case AggSum(List(), e)              => cost(e) + cardinality(e)
-      case AggSum(v, e)                   => cost(e) + cardinality(e) * searchCost(cardinality(e)) + cardinality(e) //TODO we should multiply sorting part to a constant if we know what algorithm is used
-      case _: Rel                         => cardinality(exp)
-      case Cmp(c, first, second)          => max(cardinality(first), cardinality(second)) //TODO : +they should be same size ? +can be a part of CalcSum ?
+      case CalcNeg(e)                     => cost(e) + cardinality(e)._2
+      case AggSum(List(), e)              => cost(e) + cardinality(e)._2
+      case AggSum(v, e)                   => cost(e) + cardinality(e)._2 * searchCost(cardinality(e)._2) + cardinality(e)._2 //TODO we should multiply sorting part to a constant if we know what algorithm is used
+      case _: Rel                         => cardinality(exp)._2
+      case Cmp(c, first, second)          => max(cardinality(first)._2, cardinality(second)._2) //TODO : +they should be same size ? +can be a part of CalcSum ?
       case External(_, inps, outs, tp, _) => ???
       case CmpOrList(v, consts)           => ???
       case Lift(vr, e)                    => ???
-      case Exists(term)                   => cardinality(term)
+      case Exists(term)                   => cost(term)
       case CalcValue(v: ArithFunc)        => 5.0
-      case CalcValue(v: ArithVar)         => cardinality(v)
+      case CalcValue(v: ArithVar)         => cardinality(v)._2
       case CalcValue(_)                   => 1.0
       case ArithVar(v)                    => schema.stats.getCardinalityOrElse("PART", 1) //TODO it should change finding size of R in schema
+      case _: ArithFunc                   => 5.0 //TODO it should change finding size of R in schema
     }
-    println(prettyprint(exp))
-    println("cost is : " + r)
+    //    println(prettyprint(exp))
+    //    println("cost is : " + r)
     r
   }
 
-  def cardinality(exp: CalcExpr): Double = {
+  def cardinality(exp: CalcExpr): (Int, Double) = {
     exp match {
-      case CalcProd(lst) => lst.foldLeft(1.0)((a, b) => a * cardinality(b))
-      case CalcSum(lst)  => lst.foldLeft(0.0)((a, b) => max(a, cardinality(b)))
-      case CalcNeg(e)    => cardinality(e)
-      case AggSum(v, e) => min(cardinality(e),
-        v.foldLeft(1)((a, b) => schema.stats.getDistinctAttrValuesOrElse(b.name, cardinality(e).toInt) * a))
-      case Rel(_, name, _, _)             => schema.stats.getCardinalityOrElse("PART", 1) //TODO it should change to name
-      case Cmp(c, first, second)          => max(cardinality(first), cardinality(second)) * 1 // TODO : +we can have some inference here +they should be same size I think
+      case CalcProd(lst) => (lst.foldLeft(1)((a, b) => max(a, cardinality(b)._1)), lst.foldLeft(1.0)((a, b) => {
+        if (cardinality(b)._1 == 2)
+          a * cardinality(b)._2
+        else
+          a
+      }))
+      case CalcSum(lst) => (lst.foldLeft(1)((a, b) => max(a, cardinality(b)._1)),
+        lst.foldLeft(0.0)((a, b) => max(a, cardinality(b)._2)))
+      case CalcNeg(e)        => cardinality(e)
+      case AggSum(List(), e) => (1, 1)
+      case AggSum(v, e) => (cardinality(e)._1, min(cardinality(e)._2,
+        v.foldLeft(1)((a, b) => schema.stats.getDistinctAttrValuesOrElse(b.name, cardinality(e)._2.toInt) * a)) * 0.5)
+      case Rel(_, name, _, _)             => (2, schema.stats.getCardinalityOrElse("PART", 1)) //TODO it should change to name
+      case Cmp(c, first, second)          => (1, max(cardinality(first)._2, cardinality(second)._2) * 1) // TODO : +we can have some inference here +they should be same size I think
       case External(_, inps, outs, tp, _) => ???
       case CmpOrList(v, consts)           => ???
       case Lift(vr, e)                    => ???
-      case Exists(term)                   => cardinality(term)
+      case Exists(term)                   => (1, 1)
       case CalcValue(v: ArithVar)         => cardinality(v)
-      case CalcValue(_)                   => 1
-      case ArithVar(v)                    => schema.stats.getCardinalityOrElse("PART", 1) //TODO it should change finding size of R in schema
+      case CalcValue(_)                   => (1, 1)
+      case ArithVar(v)                    => (1, schema.stats.getCardinalityOrElse("PART", 1)) //TODO it can by change finding size of R in schema
+      case _: ArithConst                  => (1, 1)
+      case _: ArithFunc                   => (1, 1)
     }
   }
 }
--- components/src/test/scala/ch/epfl/data/dblab/frontend/optimizer/CalcSearchTest.scala
+++ components/src/test/scala/ch/epfl/data/dblab/frontend/optimizer/CalcSearchTest.scala
@@ -10,19 +10,48 @@ import org.scalatest._
 import CalcUtils._
 import sc.pardis.search.BFS
 import analyzer.CalcCosting
+import ch.epfl.data.dblab.frontend.parser.CalcParser
+import ch.epfl.data.dblab.schema.TPCHSchema
 
 class CalcSearchTest extends FlatSpec {
 
-  val costEngine = CalcCosting
+  val tpchSchema = TPCHSchema.getSchema("experimentation/dbtoaster/queries/sf0.001/", 0.0001)
+  val costEngine = new CalcCosting(tpchSchema)
   val calcTransformationRules = new CalcRuleBasedTransformationRules(CalcRules.allRules)
   val bfs = new BFS(costEngine, calcTransformationRules) // can be changed with other search algorithms
 
   "CalcSearch" should "optimize query with multiple constants in prod" in {
     val optimizer = CalcOptimizer
     val r = CalcProd(List(CalcValue(ArithConst(IntLiteral(2))), CalcValue(ArithConst(IntLiteral(3))), CalcValue(ArithConst(IntLiteral(4))), CalcValue(ArithConst(IntLiteral(5))), CalcSum(List(CalcValue(ArithConst(IntLiteral(3))), CalcValue(ArithConst(IntLiteral(0))), CalcValue(ArithConst(IntLiteral(4)))))))
-    val res = bfs.search(r, 5)
-    println(res)
+    //    val r = CalcSum(List(CalcValue(ArithConst(IntLiteral(3))), CalcValue(ArithConst(IntLiteral(0))), CalcValue(ArithConst(IntLiteral(4)))))
+    println(prettyprint(r))
+    val res = bfs.search(r, 50)
+    println(prettyprint(res.asInstanceOf[CalcExpr]))
     res should not be None
+  }
+
+  import CalcRules._
+  val ruleBasedOptimizer = new CalcRuleBasedTransformer(allRules)
+
+  "CalcSearch" should "optimize simple queries similar to the rule-based version" in {
+    val parser = CalcParser
+    val folder = "experimentation/dbtoaster/queries/calcsimple/ok_raw"
+    val files = getCalcFiles(folder)
+    for (file <- files) {
+      println(s"optimizing $file")
+      val queries = parser.parse(scala.io.Source.fromFile(file).mkString)
+      for (q <- queries) {
+        //        println(q)
+        val o1 = bfs.search(q.asInstanceOf[CalcQuery].expr, 200).asInstanceOf[CalcExpr]
+        val o2 = ruleBasedOptimizer(q).asInstanceOf[CalcQuery].expr
+        println(prettyprint(q))
+        println(" " + prettyprint(o1))
+        println("cost is : " + costEngine.cost(o1))
+        println(" " + prettyprint(o2))
+        println("cost is : " + costEngine.cost(o2))
+        o1 should be(o2)
+      }
+    }
   }
 
 }
