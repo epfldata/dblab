7c83c5ee65a301153261ecb484af39a96cb278f0
Michal Pleskowicz <plaskacz.panda@gmail.com>
2018-06-08 13:44:50 +0200

    Moved the visualize method

--- components/src/main/scala/ch/epfl/data/dblab/queryengine/QueryInterpreter.scala
+++ components/src/main/scala/ch/epfl/data/dblab/queryengine/QueryInterpreter.scala
@@ -5,6 +5,7 @@ package queryengine
 import frontend.parser._
 import frontend.optimizer._
 import frontend.analyzer._
+import frontend.visualizer._
 import java.io.PrintStream
 import java.io.PrintWriter
 import java.io.File
@@ -80,14 +81,10 @@ object QueryInterpreter {
       // new SQLAnalyzer(schema).checkAndInfer(typedQuery)
       val selingerOptimizer = new SelingerOptimizer(schema)
       val operatorTree = new SQLToQueryPlan(schema).convert(typedQuery)
-      val simpleOperatorTree = new QueryPlanTree(selingerOptimizer.simplifyJoinTree(operatorTree.rootNode), new ArrayBuffer())
 
       val costingPlan = new PlanCosting(schema, operatorTree)
-      val costingPlanJoins = new PlanCosting(schema, simpleOperatorTree)
-      visualize(operatorTree, costingPlan, queryName + "_normal", false)
-      //visualize(simpleOperatorTree, costingPlanJoins, queryName + "_before")
-      System.out.println("Normal cost: " + costingPlanJoins.cost())
-      System.out.println(operatorTree)
+      DOTvisualizer.visualize(operatorTree, costingPlan, queryName + "_normal", false)
+      System.out.println("Normal cost: " + costingPlan.cost())
 
       if (Config.debugQueryPlan)
         System.out.println("Before Optimizer:\n" + operatorTree + "\n\n")
@@ -97,17 +94,14 @@ object QueryInterpreter {
       System.out.println("End of selinger optimizing")
       val selingerCosting = new PlanCosting(schema, selingerOptimizerTree)
       //System.out.println("Optimized joins: \n" + selingerOptimizerTree)
-      visualize(selingerOptimizerTree, selingerCosting, queryName + "_after", false)
-      visualize(selingerOptimizerTree, selingerCosting, queryName + "_after", true)
-      System.out.println("Optimized cost: " + selingerCosting.cost())
+      DOTvisualizer.visualize(selingerOptimizerTree, selingerCosting, queryName + "_selinger", true)
+      //System.out.println("Optimized cost: " + selingerCosting.cost())
 
       val optimizerTree = new QueryPlanNaiveOptimizer(schema).optimize(operatorTree)
-      // val optimizerTree = operatorTree
       val costingPlanOptimized = new PlanCosting(schema, optimizerTree)
-      visualize(optimizerTree, costingPlanOptimized, queryName + "_optimized", false)
+      DOTvisualizer.visualize(optimizerTree, costingPlanOptimized, queryName + "_naive", true)
 
       //System.out.println(optimizerTree)
-      //costingPlanOptimized.getExpressions()
       if (Config.debugQueryPlan)
         System.out.println("After Optimizer:\n" + optimizerTree + "\n\n")
 
@@ -115,13 +109,6 @@ object QueryInterpreter {
       PlanExecutor.executeQuery(optimizerTree, schema)
 
       val resq = scala.io.Source.fromFile(getOutputName).mkString
-
-      System.out.println("Selinger result")
-      PlanExecutor.executeQuery(selingerOptimizerTree, schema)
-      System.out.println("Break1")
-      val resOpt = scala.io.Source.fromFile(getOutputName).mkString
-      System.out.println(resOpt)
-      System.out.println("Break2")
 
       System.out.println("Normal result")
       // if (Config.debugQueryPlan)
@@ -190,90 +177,6 @@ object QueryInterpreter {
       }
 
     }
-  }
-
-  def visualize(tree: QueryPlanTree, planCosting: PlanCosting, queryName: String, simple: Boolean = true, subquery: String = "") {
-
-    def parseTree(tree: OperatorNode, suffix: String = ""): String = {
-
-      def connectTwo(node1: OperatorNode, node2: OperatorNode): String = node1 match {
-        case SubqueryNode(_) | SubquerySingleResultNode(_) => "\"" + getLabel(node1, suffix) + "\" -> \"" + getLabel(node2, suffix + "_sub") + "\" [label=\"" + planCosting.size(node2) + "\"];\n"
-        case _ => "\"" + getLabel(node1, suffix) + "\" -> \"" + getLabel(node2, suffix) + "\" [label=\"" + planCosting.size(node2) + "\"];\n"
-      }
-
-      tree match {
-        case ScanOpNode(_, _, _)                 => ""
-        case UnionAllOpNode(top, bottom)         => connectTwo(tree, top) + connectTwo(tree, bottom) + parseTree(top, suffix) + parseTree(bottom, suffix)
-        case SelectOpNode(parent, _, _)          => connectTwo(tree, parent) + parseTree(parent, suffix)
-        case JoinOpNode(left, right, _, _, _, _) => connectTwo(tree, left) + connectTwo(tree, right) + parseTree(right, suffix) + parseTree(left, suffix)
-        case AggOpNode(parent, _, _, _)          => connectTwo(tree, parent) + parseTree(parent, suffix)
-        case MapOpNode(parent, _)                => connectTwo(tree, parent) + parseTree(parent, suffix)
-        case OrderByNode(parent, ob)             => connectTwo(tree, parent) + parseTree(parent, suffix)
-        case PrintOpNode(parent, _, _)           => connectTwo(tree, parent) + parseTree(parent, suffix)
-        case SubqueryNode(parent)                => connectTwo(tree, parent) + parseTree(parent, suffix + "_sub")
-        case SubquerySingleResultNode(parent)    => connectTwo(tree, parent) + parseTree(parent, suffix + "_sub")
-        case ProjectOpNode(parent, _, _)         => connectTwo(tree, parent) + parseTree(parent, suffix)
-        case ViewOpNode(parent, _, name)         => connectTwo(tree, parent) + parseTree(parent, suffix)
-      }
-    }
-
-    def getLabel(node: OperatorNode, suffix: String = ""): String = {
-      node match {
-        case ScanOpNode(_, scanOpName, _)             => "SCAN " + scanOpName + suffix
-        case UnionAllOpNode(_, _)                     => "U" + suffix
-        case SelectOpNode(_, cond, _)                 => "Select " + cond.toString + suffix
-        case JoinOpNode(_, _, clause, joinType, _, _) => joinType + " " + clause.toString + suffix
-        case AggOpNode(_, aggs, gb, _)                => "Aggregate " + aggs.toString + " group by " + gb.map(_._2).toString + suffix
-        case MapOpNode(_, mapIndices)                 => "Map " + mapIndices.map(mi => mi._1 + " = " + mi._1 + " / " + mi._2).mkString(", ").toString + suffix
-        case OrderByNode(_, ob)                       => "Order by " + ob.map(ob => ob._1 + " " + ob._2).mkString(", ").toString + suffix
-        case PrintOpNode(_, projNames, limit) => "Project " + projNames.map(p => p._2 match {
-          case Some(al) => al
-          case None     => p._1
-        }).mkString(",").toString + {
-          if (limit != -1) ", limit = " + limit.toString
-          else ""
-        } + suffix
-        case SubqueryNode(parent)                        => "Subquery" + suffix
-        case SubquerySingleResultNode(parent)            => "Single subquery" + suffix
-        case ProjectOpNode(_, projNames, origFieldNames) => "Project " + (projNames zip origFieldNames).toString + suffix
-        case ViewOpNode(_, _, name)                      => "View " + name + suffix
-      }
-    }
-
-    def simplify(node: OperatorNode, suffix: String = ""): String = node match {
-      case ScanOpNode(_, scanOpName, _)                                => "\"" + getLabel(node, suffix) + "\"" + " [label=\"" + getLabel(node, "") + "\"];\n"
-      case UnionAllOpNode(top, bottom)                                 => "\"" + getLabel(node, suffix) + "\"" + " [label=\"∪\"];\n" + simplify(top, suffix) + simplify(bottom, suffix)
-      case SelectOpNode(parent, _, _)                                  => "\"" + getLabel(node, suffix) + "\"" + " [label=\"σ\"];\n" + simplify(parent, suffix)
-      case JoinOpNode(left, right, _, joinType, leftAlias, rightAlias) => "\"" + getLabel(node, suffix) + "\"" + " [label=\"⋈: " + joinType + "\"];\n" + simplify(left, suffix) + simplify(right, suffix)
-      case AggOpNode(parent, _, _, _)                                  => "\"" + getLabel(node, suffix) + "\"" + " [label=\"Γ\"];\n" + simplify(parent, suffix)
-      case MapOpNode(parent, _)                                        => "\"" + getLabel(node, suffix) + "\"" + " [label=\"X\"];\n" + simplify(parent, suffix)
-      case OrderByNode(parent, ob)                                     => "\"" + getLabel(node, suffix) + "\"" + " [label=\"SORT\"];\n" + simplify(parent, suffix)
-      case PrintOpNode(parent, _, _)                                   => "\"" + getLabel(node, suffix) + "\"" + " [label=\"Print\"];\n" + simplify(parent, suffix)
-      case SubqueryNode(parent)                                        => "\"" + getLabel(node, suffix) + "\"" + " [label=\"Subquery\"];\n" + simplify(parent, suffix + "_sub")
-      case SubquerySingleResultNode(parent)                            => "\"" + getLabel(node, suffix) + "\"" + " [label=\"Subquery single\"];\n" + simplify(parent, suffix + "_sub")
-      case ProjectOpNode(parent, _, _)                                 => "\"" + getLabel(node, suffix) + "\"" + " [label=\"π\"];\n" + simplify(parent, suffix)
-      case ViewOpNode(parent, _, name)                                 => "\"" + getLabel(node, suffix) + "\"" + " [label=\"View\"];\n" + simplify(parent, suffix)
-    }
-
-    val dateFormat = new SimpleDateFormat("MM-dd_HH-mm-ss");
-
-    val date = new Date()
-
-    val graphSuffix = if (simple) "_simple" else "_full"
-    val fileName = queryName + "_" + dateFormat.format(date) + graphSuffix
-
-    val pw = new PrintWriter(new File("/Users/michal/Desktop/SemesterProject/visualisations/" + fileName + ".gv"))
-
-    pw.write("digraph G { \n	size=\"8,8\"\nrankdir = TB;\nedge[dir=back];\n")
-
-    var result = ""
-
-    //result += "\"Cost\" [label=\"Cost: " + planCosting.cost() + "\"];\n";
-    if (simple) result += simplify(tree.rootNode)
-    result += parseTree(tree.rootNode)
-    pw.write(result)
-    pw.write("}")
-    pw.close
   }
 
   def addStats(tpchSchema: Schema): Schema = {
@@ -397,15 +300,6 @@ object QueryInterpreter {
     newSchema.stats += "CARDINALITY_PART" -> partTable.rowCount * SCALING_FACTOR
     newSchema.stats += "CARDINALITY_NATION" -> nationTable.rowCount
     newSchema.stats += "CARDINALITY_REGION" -> regionTable.rowCount
-    /*
-    System.out.println("CARDINALITY_ORDERS: " + ordersTable.rowCount)
-    System.out.println("CARDINALITY_CUSTOMER: " + customerTable.rowCount)
-    System.out.println("CARDINALITY_LINEITEM: " + lineItemTable.rowCount)
-    System.out.println("CARDINALITY_SUPPLIER: " + supplierTable.rowCount)
-    System.out.println("CARDINALITY_PARTSUPP: " + partsuppTable.rowCount)
-    System.out.println("CARDINALITY_PART: " + partTable.rowCount)
-    System.out.println("CARDINALITY_NATION: " + nationTable.rowCount)
-    System.out.println("CARDINALITY_REGION: " + regionTable.rowCount)*/
 
     newSchema.stats += "CARDINALITY_Q1GRP" -> 4
     newSchema.stats += "CARDINALITY_Q3GRP" -> ordersTable.rowCount / 100
